ec68cd73f0d8adfa707dc97d2193465484bc42c1
neelkRemalli <aboneel1971@gmail.com>
2025-10-24 17:17:54 +0200

    install eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tailwindPlugin from 'eslint-plugin-tailwindcss'
+import prettier from 'eslint-config-prettier'
+
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  tailwindPlugin.configs.recommended,
+  prettier,
+ 
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
